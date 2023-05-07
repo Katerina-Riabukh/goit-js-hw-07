881f0f5fcf493d8a59a2dc53434450fffc2c1cfe
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-05-07 16:05:32 +0200

    1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+const listMarcap = galleryItems.map(({ preview, original, description }) => {
+
+        return ` <li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </li>`
+   
+}).join('')
+
+list.insertAdjacentHTML("afterbegin", listMarcap);
+
+list.addEventListener('click', showOriginalSize);
+
+function showOriginalSize(event) {
+    event.preventDefault();
+    //console.log(event.target);
+    //console.log(event.currentTarget);
+    const urlOriginal = event.target.dataset.source;
+    const modal = basicLightbox.create(`<img src="${urlOriginal}">`)
+    if (event.target !== event.currentTarget) {
+        modal.show()
+    } 
+
+    window.addEventListener('keydown', closeOriginalSizeByEsc);
+    function closeOriginalSizeByEsc(event) {
+         
+        if (event.code !== 'Escape') {
+              return
+        }
+
+     window.removeEventListener('keydown', closeOriginalSizeByEsc)
+     modal.close()
+       
+     console.log(event.code);
+    }
+
+}
+
+
+
+
+// function closeOriginalSizeByEsc(event) {
+//     // modal.close()
+//     console.log(event);
+// }
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+
+const list = document.querySelector('.gallery');
+
+const listMarcap = galleryItems.map(({ preview, original, description }) => {
+
+    return ` <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}" />
+                </a>
+            </li>`
+   
+}).join('');
+
+list.insertAdjacentHTML("afterbegin", listMarcap);
+
+// list.addEventListener('click', showPhotoSlider);
+
+// function showPhotoSlider(event) {
+//     event.preventDefault();
+
+// };
+
+ const lightbox = new SimpleLightbox('.gallery a', { animationSpeed: 150,captionsData: 'alt', captionDelay: 250,});
+
+
