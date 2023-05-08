d18dca1b41c288f21c9794da92817879b8eda287
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-05-08 22:18:32 +0200

    :)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -26,8 +26,6 @@ list.addEventListener('click', showOriginalSize);
 function showOriginalSize(event) {
     event.preventDefault();
 
-    console.log(event.target);
-
     if (event.target.nodeName !== "IMG") {
         return
     }
@@ -49,5 +47,62 @@ function showOriginalSize(event) {
         modal.close();
     }
 }
+
+//------------ Variant #1 -----------------------------------//
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// const list = document.querySelector('.gallery');
+
+
+// const listMarcap = galleryItems.map(({ preview, original, description }) => {
+
+//         return ` <li class="gallery__item">
+//                     <a class="gallery__link" href="${original}">
+//                         <img
+//                         class="gallery__image"
+//                         src="${preview}"
+//                         data-source="${original}"
+//                         alt="${description}"
+//                         />
+//                     </a>
+//                 </li>`
+   
+// }).join('')
+
+// list.insertAdjacentHTML("afterbegin", listMarcap);
+
+// list.addEventListener('click', showOriginalSize);
+
+
+//  function showOriginalSize(event) {
+//     event.preventDefault();
+  
+//     const urlOriginal = event.target.dataset.source;
+//     const modal = basicLightbox.create(`<img src="${urlOriginal}">`,{closable: true})
+//     if (event.target !== event.currentTarget) {
+//         modal.show()
+//     } 
+
+//     window.addEventListener('keydown', closeOriginalSizeByEsc);
+//     function closeOriginalSizeByEsc(event) {
+//          console.log(event.code);
+
+//         if (event.code !== 'Escape') {
+//             return
+//         }
+//         window.removeEventListener('keydown', closeOriginalSizeByEsc)
+//         modal.close();
+       
+     
+//     }
+
+  
+// }
+
+//-----------------------------------------------------------------------------------//
+
+
 
 
